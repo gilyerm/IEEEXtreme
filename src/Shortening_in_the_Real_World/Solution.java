ce5990e99f3488ce2dfbc44267f2a0164238f213
Unknown <giladanon@gmail.com>
2019-04-19 00:47:41 +0300

    Works! (100), cleaned-up & submit
    gilad's cs user @ hackerrank:
    https://www.hackerrank.com/contests/ieeextreme-challenges/challenges/shortening-in-the-real-world/submissions/code/1313935670

--- src/Shortening_in_the_Real_World/Solution.java
+++ src/Shortening_in_the_Real_World/Solution.java
@@ -2,10 +2,8 @@ package Shortening_in_the_Real_World;
 
 import java.math.BigInteger;
 import java.nio.charset.Charset;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class Solution {
 
@@ -15,13 +13,11 @@ public class Solution {
 	static Scanner scan = new Scanner(System.in);
 
 	public static void main(String... args) {
-		String baseURL = scan.nextLine();
+		final String baseURL = scan.nextLine();
 		byte[] baseURLArr = asByteArr(baseURL);
 		int tests = scan.nextInt(); scan.nextLine();
 		for (int t = 0; t < tests; t++) {
 			byte[] targetURLArr = asByteArr(scan.nextLine());
-			logArr("B_inp", baseURLArr);
-			logArr("T_inp", targetURLArr);
 			byte[] xor = xORUrls(baseURLArr, targetURLArr);
 			System.out.printf("%s/%s\n", baseURL, toBase62(xor));
 		}
@@ -48,32 +44,7 @@ public class Solution {
 		for (int i=0; i<8; i++) {	// xor the 2 8-byte ranges
 			xor[i] = (byte) (target8[i] ^ base8[i]);
 		}
-
-		logThing("--------");
-		logThing(String.format("f=%d, l=%d", last-8, last));
-		logArr("B_full", baseURLArr);
-		logArr("T_full", targetURLArr);
-
-		logThing("--------");
-		logArr("B_8", base8);
-		logArr("T_8", target8);
-
 		return xor;
-	}
-
-	public static void logArr(String title, byte[] arr) {
-		logThing(String.format("%s %s (%d)", title, ArrayToString(arr), arr.length));
-	}
-	public static void logThing(String s) {
-		// System.out.println(s);
-	}
-	public static String ArrayToString(byte[] arr) {
-		ArrayList<Byte> arrayList = new ArrayList<>();
-		for (byte b : arr) arrayList.add(b);
-		String list = arrayList.stream()
-				.map(b -> String.format("%03d",(int)(b)))
-				.collect(Collectors.joining(", "));
-		return String.format("[%s]",list);
 	}
 
 	public static String toBase62(byte[] xor) {
