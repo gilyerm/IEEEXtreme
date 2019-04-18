a10743c6241a0c90691ccdded4ba04e312b24c9a
Unknown <giladanon@gmail.com>
2019-04-18 15:57:52 +0300

    recreation - restoring progress

--- src/Shortening_in_the_Real_World/Solution.java
+++ src/Shortening_in_the_Real_World/Solution.java
@@ -1,69 +1,90 @@
 package Shortening_in_the_Real_World;
 
+import java.math.BigInteger;
 import java.nio.charset.Charset;
-import java.util.Arrays;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Scanner;
 
+// https://raw.githubusercontent.com/ieeesbspain/IEEEXtreme/master/challenges/ieeextreme9/P20%20-%20Shortening%20in%20the%20Real%20World/CraftCoders/Solution/Program.cs
 public class Solution {
 
-	public static String alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	public static String alphabetBase = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	public static void main(String[] args)
 	{
 		Scanner scan = new Scanner(System.in);
-		String base = scan.nextLine();
+		String baseURL = scan.nextLine();
 		int tests = scan.nextInt();
 		scan.nextLine();
 		for (int i=0; i<tests; i++) {
-			String url = scan.nextLine();
-			int start,end;
-			// assume base.len < url.len
-			start = url.length() % base.length();
-			end = 8-start;
-			int lastBase = base.length(), lastURL=url.length();
-			String subBase =
-					base.substring(lastBase-end, lastBase) + base.substring(0,start);
-			String subURL = url.substring(lastURL-8, lastURL);
-			byte[] bytesBase = subBase.getBytes(Charset.forName("UTF-8")),
-					bytesURL =  subURL.getBytes(Charset.forName("UTF-8"));
+			String targetURL = scan.nextLine();
+			int baseURLLen = baseURL.getBytes().length,
+					targetURLLen = targetURL.getBytes().length;
 
-//			System.out.println(subBase + Arrays.toString(bytesBase));
-//			System.out.println(subURL + Arrays.toString(bytesURL));
+//			byte[] allBytesBaseURL = baseURL.getBytes(Charset.forName("UTF8")),
+//				 allBytesTargetURL = targetURL.getBytes(Charset.forName("UTF8"));
+			byte[]  allBytesBaseURL = StandardCharsets.UTF_8.encode(baseURL).array(),
+					allBytesTargetURL = StandardCharsets.UTF_8.encode(targetURL).array();
 
-			long base10Val = 0;
-			for (int j=0; j<7; j++) {
-				byte b = (byte) (bytesBase[j] ^ bytesURL[j]);
+			ArrayList<Byte>
+					bytesBaseURL = new ArrayList<>(),
+					bytesTargetURL = new ArrayList<>();
+
+			if (baseURLLen < targetURLLen) {	// System.out.println("baseURL < tgt");
+				int start,end;
+				start = targetURLLen % baseURLLen;
+				end = 8 - start % 8;
+
+				int baseStart = baseURLLen - end,
+						baseEnd = baseURLLen;
+				for (int j=baseStart; j<baseEnd; j++)
+					bytesBaseURL.add(allBytesBaseURL[j]);
+				for (int j=0; j<start; j++)
+					bytesBaseURL.add(allBytesBaseURL[j]);
+
+				int targetStart = targetURLLen - 8,
+						targetEnd = targetURLLen;
+				for (int j=targetStart; j<targetEnd; j++)
+					bytesTargetURL.add(allBytesTargetURL[j]);
+			}
+			else {	// System.out.println("baseURL >= tgt");
+				int start,end;
+				end = targetURLLen;
+				start = targetURLLen - 8;
+				for (int j=start; j<end; j++) {
+					bytesBaseURL.add(allBytesBaseURL[j]);
+					bytesTargetURL.add(allBytesTargetURL[j]);
+				}
+			}
+
+//			System.out.println(subBase + Arrays.toString(bytesBaseURL));
+//			System.out.println(subTarget + Arrays.toString(bytesURL));
+
+			BigInteger base10Val = BigInteger.ZERO;
+			int j;
+			for (j=0; j<7; j++) {
+				byte b = (byte) (bytesBaseURL.get(j) ^ bytesTargetURL.get(j));
 //				System.out.printf("%02x,",b);
-				base10Val += b;
-				base10Val <<= 8;
+				base10Val = base10Val.add(BigInteger.valueOf(b));
+				base10Val = base10Val.shiftLeft(8);
 			}
-			byte b = (byte) (bytesBase[7] ^ bytesURL[7]);
-			base10Val += b;
+			byte b = (byte) (bytesBaseURL.get(j) ^ bytesTargetURL.get(j));
+			base10Val = base10Val.add(BigInteger.valueOf(b));
 //			System.out.println("\n"+base10Val);
+//			System.out.println(alphabetBase.length()+" "+alphabetBase);
 
-//			System.out.println(alphabet.length()+" "+alphabet);
 			StringBuilder sb = new StringBuilder();
-			while (base10Val != 0) {
-				int mod = Integer.parseInt(base10Val % 62 + "");
-				char digit = alphabet.charAt(mod);
+			while (base10Val.compareTo(BigInteger.ZERO) != 0) {
+				int mod = base10Val.mod(BigInteger.valueOf(62)).intValue();
+				char digit = alphabetBase.charAt(mod);
 				sb.append(digit);
-				base10Val /= 62l;
+				base10Val = base10Val.divide(BigInteger.valueOf(62));
 			}
+			if (sb.length() == 0)
+				sb.append(0);
 
-			System.out.printf("%s/%s\n", base, sb.reverse().toString());
+			System.out.printf("%s/%s\n", baseURL, sb.reverse().toString());
 		}
 
 	}
 }
-
-// http://www.ieee.comhttp://
-// http://www.ieee.org/xtreme
-
-// http://www.ieee.comhttp://www.ieee.comhttp://www.ieee.comhttp://www.ieee.comhttp://www.ieee.com
-// http://www.ieee.org/membership_services/membership/young_professionals/index.html
-
-// http://www.ieee.commhttp://www.ieee.commhttp://www.ieee.commhttp://www.ieee.commh
-// http://www.ieee.org/membership_services/membership/young_professionals/index.html
-
-
-// http://giladsawsomewesite.com
-// http://gil.hi
