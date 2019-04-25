4ecc68df4ff071657ff38f83f916dff1f86e8717
Unknown <giladanon@gmail.com>
2019-04-25 15:35:34 +0300

    Works! (100) cleaned-up & submitted
    solved (25.4.19). uploaded to gilad's @ csacademy:
    https://csacademy.com/submission/2201242
    https://csacademy.com/ieeextreme-practice/task/d48ada9a7213299f1b24b22b2fb9443f/

--- src/Aeneas_cryptographic_disc/Solution.java
+++ src/Aeneas_cryptographic_disc/Solution.java
@@ -1,74 +1,84 @@
 package Aeneas_cryptographic_disc;
-
 import java.util.*;
 import java.lang.*;
 
-// Please name your class Main
-public class Solution {
-	static Scanner scan = new Scanner(System.in);
-	static HashMap<Character, Point> points = new HashMap<>();
-	public static void main (String[] args) {
-		double r = scan.nextDouble();
-// 		System.out.println(r);
-		for (int i=0; i<26; i++) {
-			Character letter = scan.next().charAt(0);
-			double angle = scan.nextDouble();
-// 			System.out.printf("%c %f\n", letter, angle);
-			points.put(letter, new Point(r, angle));
-		}
-// 		points.entrySet().stream().forEach(System.out::println);
+public class Solution   // Please name your class Main
+{
+	static Disc disc;
+	static double r;
+	static char[] messageArr;
 
+	static Scanner scan = new Scanner(System.in);
+	public static void main (String[] args) {
+		getAllInputs();
+		ArrayList<Point> path = tracePointPath(messageArr);
+		double distance = totalThreadDistance(path);
+		long result = (long) Math.ceil(distance);
+		System.out.println(result);
+	}
+
+	public static void getAllInputs() {
+		disc = new Disc();
+		r = scan.nextDouble();
+		for (int i=0; i<26; i++)
+			disc.setPoint(
+				scan.next().charAt(0),	// letter to set
+				new Point(r, scan.nextDouble())	// at point(r,angle)
+			);
 		StringBuilder sb = new StringBuilder();
 		while(scan.hasNext()) sb.append(scan.next());
-		String message = sb.toString().replaceAll("[^a-zA-Z]", "").toUpperCase();
-// 		System.out.println(message);
-		System.out.println((long)Math.ceil(calcThreadLength(message)));
-// 		System.out.println();
-// 		System.out.println(points.get('G').distance(
-// 		    points.get('R')
-// 		    ));
+		messageArr = filterMessageLetters(sb.toString());
 	}
 
-	static double calcThreadLength(String message) {
-		// double length = 0;
-		char[] arr = message.toCharArray();
+	public static char[] filterMessageLetters(String message) {
+		return message
+			.replaceAll("[^a-zA-Z]", "") // only alphabetical chars
+			.replaceAll("(.)\\1+", "$1") // no consecutive repeating chars
+			.toUpperCase() 		// all letters to upper case
+			.toCharArray();		// keep as char array
+	}
+
+	static ArrayList<Point> tracePointPath(char[] letters) {
 		ArrayList<Point> path = new ArrayList<>();
 		path.add(Point.CENTER);
-		char prev='\0';
-		for (int i=0; i < arr.length; i++) {
-			char cur = arr[i];
-			if (prev == cur)
-				continue; //path.add(Point.CENTER);
-			path.add(points.get(cur));
-			prev = cur;
-		}
-// 		path.add(Point.CENTER);
-		// System.out.println(path.size());
-		return totalDistance(path);
+		for (char l : letters)
+			path.add(disc.getPoint(l));
+		return path;
 	}
 
-	public static double totalDistance(ArrayList<Point> path) {
+	public static double totalThreadDistance(ArrayList<Point> path) {
 		double distance = 0;
 		for (int i=0; i<path.size()-1; i++)
 			distance += path.get(i).distance(path.get(i+1));
 		return distance;
 	}
 
-}
+	static class Disc
+	{
+		Point[] abcPoints = new Point[26];
+		public void setPoint(char letter, Point point) {
+			abcPoints[index(letter)] = point;
+		}
+		public Point getPoint(char letter) {
+			return abcPoints[index(letter)];
+		}
+		public int index(char letter){
+			return letter - 'A';
+		}
+	}
 
-class Point {
-	double x,y;
-	Point(double r, double angle) {
-		double rad = Math.toRadians(angle);
-		this.x = Math.cos(rad) * r;
-		this.y = Math.sin(rad) * r;
+	static class Point
+	{
+		double x,y;
+		Point(double r, double angle) {
+			double rad = Math.toRadians(angle);
+			this.x = Math.cos(rad) * r;
+			this.y = Math.sin(rad) * r;
+		}
+		public static Point CENTER = new Point(0,0);
+		public double distance(Point p) {
+			return Math.hypot( Math.abs(this.x - p.x), Math.abs(this.y - p.y) );
+		}
 	}
-	public static Point CENTER = new Point(0,0);
-	public double distance(Point p) {
-		return Math.hypot(Math.abs(this.x - p.x), Math.abs(this.y - p.y));
-	}
-	@Override
-	public String toString() {
-		return String.format("{x=%f,y=%f}",x,y);
-	}
+
 }
